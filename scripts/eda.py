93fefebede1eb13a72d0b180a6f83aac917494e6
Yi Han <yi.han.178@gmail.com>
2023-11-30 23:03:12 -0800

    checked. successfully produce figures.

--- scripts/eda.py
+++ scripts/eda.py
@@ -6,21 +6,29 @@ import click
 import os
 import pandas as pd
 import altair as alt
-import dataframe_image as dfi
+import matplotlib.pyplot as plt
+import seaborn as sns
 
 @click.command()
 @click.option('--data-file', type=str, help="Path to the dataset")
 @click.option('--plot-to', type=str, help="Path to directory where the plots will be written to")
 
-def main(data_file, output_path):
-    '''Performs EDA on precipitation data and saves histograms and correlation table as PNG files.'''
+def main(data_file, plot_to):
+    '''Performs EDA on precipitation data and saves histograms and 
+    the correlation table as PNG files.'''
 
     # Read the data and preprocess
-    precipit_df = pd.read_csv(data_file).drop(columns = ['sunrise', 'sunset', 'weather_code', 'rain_sum', 'snowfall_sum', 'precipitation_hours', 'date'])
-
-    alt.data_transformers.disable_max_rows()
+    precipit_df = pd.read_csv(data_file).drop(columns = ['sunrise', 
+                                                         'sunset', 
+                                                         'weather_code', 
+                                                         'rain_sum', 
+                                                         'snowfall_sum',
+                                                         'precipitation_hours', 
+                                                         'date'])
 
     # Generate histograms for numeric columns
+    alt.data_transformers.disable_max_rows()
+    
     numeric_cols = precipit_df.select_dtypes(include=['number']).columns.tolist()
 
     numeric_cols_hists = alt.Chart(precipit_df).mark_bar().encode(
@@ -34,14 +42,16 @@ def main(data_file, output_path):
         columns=4
     )
     # Save histograms as PNG
-    numeric_cols_hists.save(f'{output_path}/numeric_cols_hists.png', scale_factor=2.0)
+    numeric_cols_hists.save(os.path.join(plot_to, "feature_densities_by_class.png"),
+              scale_factor=2.0)
 
     # Generate and save correlation table
     correlation_table = precipit_df[numeric_cols].corr(method='spearman')
     plt.figure(figsize=(10, 8))
     sns.heatmap(correlation_table, annot=True, cmap="gist_yarg")
     plt.title("Correlation Table")
-    plt.savefig(f'{output_path}/correlation_table.png')
+    plt.savefig(os.path.join(plot_to, "correlation_heatmap.png"))
 
+    
 if __name__ == '__main__':
     main()
