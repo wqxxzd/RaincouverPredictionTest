b63bd0536cec4a6b705c0d913f0a34fb4bba597f
sivakornchong <sivakornchong@gmail.com>
2023-12-01 20:43:24 -0800

    done with classification file

--- scripts/classification.py
+++ scripts/classification.py
@@ -14,6 +14,10 @@ from sklearn.tree import DecisionTreeClassifier
 from sklearn.metrics import fbeta_score, make_scorer
 from joblib import dump
 import sys
+import matplotlib.pyplot as plt
+
+from sklearn.metrics import classification_report
+from sklearn import set_config
 
 sys.path.append(os.path.join(os.path.dirname(__file__), '..'))
 from src.utils import cross_val_model
@@ -21,14 +25,16 @@ from src.utils import cross_val_model
 @click.command()
 @click.option('--x_train', type=str, help="Path to X training data")
 @click.option('--y_train', type=str, help="Path to y training data")
+@click.option('--x-test', type=str, help="Path to x test data")
+@click.option('--y-test', type=str, help="Path to y test data")
 @click.option('--preprocessor', type=str, help="Path to preprocessor object", default=None)
 @click.option('--columns-to-drop', type=str, help="Optional: columns to drop")
 @click.option('--pipeline-to', type=str, help="Path to directory where the pipeline object will be written to")
-# @click.option('--plot-to', type=str, help="Path to directory where the plot will be written to")
+@click.option('--plot-to', type=str, help="Path to directory where the plot will be written to")
 @click.option('--seed', type=int, help="Random seed", default=123)
 
-def main(x_train, y_train, preprocessor, columns_to_drop,
-         pipeline_to, seed): 
+def main(x_train, y_train, x_test, y_test, preprocessor, columns_to_drop,
+         pipeline_to, plot_to, seed): 
     '''Fits a classifier to the training data, saves pipeline object and produce relevant figures'''
     np.random.seed(seed)
     set_config(transform_output="pandas")
@@ -36,8 +42,8 @@ def main(x_train, y_train, preprocessor, columns_to_drop,
     #Read in data and preprocessor
     X_train = pd.read_csv(x_train)
     y_train = pd.read_csv(y_train)
-    y_col_name = y_train.columns.tolist()[0]
     
+    y_col_name = y_train.columns.tolist()[0]
     y_train_class = y_train[y_col_name]
     preprocess = pickle.load(open(preprocessor, "rb"))
     
@@ -45,9 +51,9 @@ def main(x_train, y_train, preprocessor, columns_to_drop,
         to_drop = pd.read_csv(columns_to_drop).feats_to_drop.tolist()
         X_train = X_train.drop(columns=to_drop)
 
-    click.echo(f'Running model training for {X_train.columns.tolist()}')
-    
-    #Model type selection
+    click.echo(f'Running model training for columns {X_train.columns.tolist()}')
+
+    #Model parameter to be explored
     models = {
     "Decision Tree": DecisionTreeClassifier(random_state=522),
     "KNN": KNeighborsClassifier(),
@@ -55,6 +61,23 @@ def main(x_train, y_train, preprocessor, columns_to_drop,
     "Logistic Regression": LogisticRegression(max_iter=2000, multi_class="ovr", random_state=522),
     }
 
+    #Plot out feature importance
+    pipe = make_pipeline(preprocess, models["Logistic Regression"])
+    pipe.fit(X_train, y_train_class)
+    numeric_features = pipe.named_steps['columntransformer'].named_transformers_['standardscaler'].get_feature_names_out().tolist()
+    coefficients = pipe.named_steps['logisticregression'].coef_[0]
+    
+    feature_importance = pd.DataFrame({'Feature': numeric_features, 'Importance': np.abs(coefficients)})
+    feature_importance = feature_importance.sort_values('Importance', ascending=True)   
+    plt.figure(figsize=(12, 10))
+    feature_importance.plot(x='Feature', y='Importance', kind='barh')
+    plt.title("Feature_importance")
+    plt.tight_layout()
+    plt.savefig(os.path.join(plot_to, "Feature_importance.png"))
+
+    click.echo(f'Feature importance has been parked at {os.path.join(plot_to, "Feature_importance.png")}')
+    
+    #Model type selection
     classification_metrics = ["accuracy", "precision", "recall", "f1"]
     results_df = cross_val_model(preprocess, models, X_train, y_train_class, classification_metrics)
 
@@ -71,7 +94,7 @@ def main(x_train, y_train, preprocessor, columns_to_drop,
     else:
         click.echo('Best model is not expected SVM RBF. Other model parameters required for optimization, exitting the program')
         sys.exit(0)
-        
+
     #Hyperparameter optimization
     param_grid = {"svc__C": 10.0**np.arange(-3,3)}
     svc_pipe = make_pipeline(preprocess, models[model_name])
@@ -88,8 +111,33 @@ def main(x_train, y_train, preprocessor, columns_to_drop,
 
     click.echo(f'Optimized model has been parked at {os.path.join(pipeline_to, "optimum_cls_svm_pipeline.pickle")}')
 
-    #Print out feature importance
+    #Saving model evaluation result
+    X_test = pd.read_csv(x_test)
+    y_test = pd.read_csv(y_test)
+    y_col_name = y_test.columns.tolist()[0]
+    y_test_class = y_test[y_col_name]
+
+    if columns_to_drop:
+        to_drop = pd.read_csv(columns_to_drop).feats_to_drop.tolist()
+        X_test = X_test.drop(columns=to_drop)
+        
+    click.echo(f'Running model test for columns {X_test.columns.tolist()}')        
+    y_pred = opt_pipe.predict(X_test)
+
+    report_dict = classification_report(y_test_class, y_pred, output_dict=True)
+    report_df = pd.DataFrame(report_dict).transpose()
     
+    report_df = report_df.loc[['False', 'True']].round(2)
+    report_df.index = ['No rain', 'Rain']
+    
+    plt.figure(figsize=(6, 6))
+    plt.table(cellText=report_df.values,
+              colLabels=report_df.columns,
+              rowLabels=report_df.index,
+              loc='center')
+    plt.axis('off')
+    plt.savefig(os.path.join(plot_to, "classification_report.png"))
+    click.echo(f'Classification report has been parked at {os.path.join(plot_to, "classification_report.png")}')
 
 if __name__ == '__main__':
     main()
